0c550f9c53594e0a9b04e4e528aa0b38ea7a4fb5
Mario <96387178+miloshevskimario@users.noreply.github.com>
2022-01-11 01:42:47 +0100

    Add files via upload

---
+++ Class 02 - VAR , LET & CONST/script.js
@@ -0,0 +1,51 @@
+let input = prompt (" Enter your birth year: ");
+console.log(input);
+let year = (input - 4) % 12;
+
+if (year === 0) {
+    console.log(" Your animal is rat ");
+}
+
+else if (year === 1) {
+    console.log(" Your animal is Ox");
+}
+
+else if (year === 2) {
+    console.log(" Your animal is Tiger");
+}
+
+else if (year === 3) {
+    console.log(" Your animal is Rabbit");
+}
+
+else if (year === 4) {
+    console.log(" Your animal is Dragon");
+}
+
+else if (year === 5) {
+    console.log(" Your animal is Snake");
+}
+
+else if (year === 6) {
+    console.log(" Your animal is Horse");
+}
+
+else if (year === 7) {
+    console.log(" Your animal is Goat");
+}
+
+else if (year === 8) {
+    console.log(" Your animal is Monkey");
+}
+
+else if (year === 9) {
+    console.log(" Your animal is Rooster");
+}
+
+else if (year === 10) {
+    console.log(" Your animal is Dog");
+}
+
+else if (year === 11) {
+    console.log(" Your animal is Pig");
+}
