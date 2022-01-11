b0aa6f6a9dd17b35367c0307d024a799edf1f4d6
Mario <96387178+miloshevskimario@users.noreply.github.com>
2022-01-11 01:40:57 +0100

    Add files via upload

---
+++ Class 01 - JS basics/script.js
@@ -0,0 +1,9 @@
+
+var phonePrice = 119.95;
+var taxRate = 5 / 10;
+var pricePlusTax = phonePrice + taxRate
+console.log(pricePlusTax)
+
+var priceOfThirtyPhones = 30 * pricePlusTax
+console.log("The price of 30 phones that cost 119.95 each with a tax rate of 5% is" +  priceOfThirtyPhones);
+
